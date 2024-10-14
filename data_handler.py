c83e3993bac645581dabc31bd1898fee5db850f8
jlmacknight <106911695+jlmacknight@users.noreply.github.com>
2024-10-14 16:59:00 +0100

    Docstring changes.

--- data_handler.py
+++ data_handler.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def load_experimental_data(filepath: str) -> tuple:
+def load_experimental_data(filepath: str) -> tuple[np.ndarray, np.ndarray]:
     """
     Load time and volume data from a CSV file.
 
@@ -9,7 +9,7 @@ def load_experimental_data(filepath: str) -> tuple:
         filepath (str): Path to the CSV file.
 
     Returns:
-        tuple: A tuple containing:
+        tuple[numpy.ndarray, numpy.ndarray]: A tuple containing:
             tdata (numpy.ndarray): Experimental time data.
             vdata (numpy.ndarray): Experimental volume data.
     """
@@ -17,4 +17,3 @@ def load_experimental_data(filepath: str) -> tuple:
     tdata = data[data.filter(like="time").columns[0]].values
     vdata = data[data.filter(like="volume").columns[0]].values
     return tdata, vdata
-
